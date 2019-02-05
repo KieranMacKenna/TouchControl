059eb88916655ee281a4805d30507efe223aa577
KieranMacKenna <kieranmac100@hotmail.com>
2019-02-05 15:36:04 +0000

    Touch position

--- Assets/Scripts/TouchControlsScript.cs
+++ Assets/Scripts/TouchControlsScript.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectionManager : MonoBehaviour
+public class TouchControlsScript : MonoBehaviour
 {
+    private Camera MainCam;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if there is any touch on the screen
+        if (Input.touchCount > 0)
+        {
+            Touch firstTouch = Input.GetTouch(0);
+            //Get the position of the touch in world space
+            Vector3 touchPosWS = MainCam.ScreenToWorldPoint(firstTouch.position);
+            Ray ray = MainCam.ScreenPointToRay(touchPosWS);
+
+            
+        }
     }
 }
